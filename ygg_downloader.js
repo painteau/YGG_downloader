292edb1ffae45753f36ee6064d1b8316032eef3c
KaO <52933590+painteau@users.noreply.github.com>
2025-04-07 20:11:26 +0200

    add key, GUID and reboot of injection method

--- ygg_downloader.js
+++ ygg_downloader.js
@@ -10,17 +10,29 @@ function checkData() {
       th.colSpan = 1;
       th.innerText = "Actions";
       table.tHead.rows[0].appendChild(th);
+      
       for (let row of table.tBodies[0].rows) {
         let td = document.createElement("td");
 
-        const link = row.cells[1].firstChild.href;
-        const name = link.substr(link.lastIndexOf("/") + 1);
+        const linkHref = row.cells[1].firstChild.href;
+        const name = linkHref.substr(linkHref.lastIndexOf("/") + 1);
         const id = name.substr(0, name.indexOf("-"));
 
-        td.innerHTML =
-          '<a href="https://www.yggtorrent.top/engine/download_torrent?id=' +
-          id +
-          '" style="background-color: #22282f; color: white; padding: 2px 8px; text-decoration: none; border-radius: 3px; display: inline-block; font-size: 0.9em; transition: background-color 0.3s;">⬇️ Download</a>';
+        let downloadLink = document.createElement("a");
+        downloadLink.href = "https://www.yggtorrent.top/engine/download_torrent?id=" + id;
+        downloadLink.style.backgroundColor = "#22282f";
+        downloadLink.style.color = "white";
+        downloadLink.style.padding = "2px 6px";
+        downloadLink.style.textDecoration = "none";
+        downloadLink.style.borderRadius = "3px";
+        downloadLink.style.display = "inline-block";
+        downloadLink.style.fontSize = "0.9em";
+        downloadLink.style.transition = "background-color 0.3s";
+
+        downloadLink.textContent = "⬇️ Download";
+
+        td.appendChild(downloadLink);
+        
         row.appendChild(td);
       }
     });
